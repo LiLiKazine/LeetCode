9bb58652e40a7b7b9658a11d587701e8b9967696
LiLi Kazine <lilikazine@gmail.com>
2020-01-17 15:47:02 +0800

    undone

---
+++ leetcode.playground/Pages/206. Reverse Linked List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+//: [Previous](@previous)
+
+/*
+ 206. Reverse Linked List
+ Easy
+
+ Reverse a singly linked list.
+
+ Example:
+
+ Input: 1->2->3->4->5->NULL
+ Output: 5->4->3->2->1->NULL
+ Follow up:
+
+ A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+ */
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        return nil
+    }
+}
+
+//: [Next](@next)
