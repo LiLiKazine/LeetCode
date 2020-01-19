f3f175b8b7a65f94aa3215bf3a98758d217295e4
LiLi Kazine <lilikazine@gmail.com>
2020-01-19 16:57:30 +0800

    undone

---
+++ leetcode.playground/Pages/217. Contains Duplicate.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+
+/*
+ 217. Contains Duplicate
+ Easy
+
+ Given an array of integers, find if the array contains any duplicates.
+
+ Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+ Example 1:
+
+ Input: [1,2,3,1]
+ Output: true
+ Example 2:
+
+ Input: [1,2,3,4]
+ Output: false
+ Example 3:
+
+ Input: [1,1,1,3,3,4,3,2,4,2]
+ Output: true
+ 
+ */
+
+import Foundation
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var nums = nums.sorted()
+        if nums.count == 0 {
+            return false
+        }
+        for i in 1..<nums.count {
+            if nums[i] == nums[i-1] {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+//: [Next](@next)
