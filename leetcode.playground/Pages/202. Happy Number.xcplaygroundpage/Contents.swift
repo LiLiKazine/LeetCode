9d7921ad14a1a9c47d0924d58e623aa3546deebc
LiLi Kazine <lilikazine@gmail.com>
2020-04-30 17:36:07 +0800

    202. Happy Number

---
+++ leetcode.playground/Pages/202. Happy Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 202. Happy Number
+ 
+ Write an algorithm to determine if a number n is "happy".
+
+ A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+ Return True if n is a happy number, and False if not.
+
+ Example:
+
+ Input: 19
+ Output: true
+ Explanation:
+ 12 + 92 = 82
+ 82 + 22 = 68
+ 62 + 82 = 100
+ 12 + 02 + 02 = 1
+ 
+ */
+
+import Foundation
+
+class Solution {
+
+    func isHappy(_ n: Int) -> Bool {
+        var record: [Int: Int] = [:]
+        var n = n
+        while n > 1 && record[n] == nil {
+            record[n] = process(n)
+            n = record[n]!
+        }
+        return n == 1
+    }
+        
+    func process(_ n: Int) -> Int {
+        var m = n, res = 0
+        while m % 10 + m / 10 > 0 {
+            let num = m % 10
+            m /= 10
+            res += num * num
+        }
+        return res
+    }
+}
+
+let solution = Solution()
+let ans = solution.isHappy(2)
+
+//: [Next](@next)
--- leetcode.playground/Pages/650. 2 Keys Keyboard(undone).xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/650. 2 Keys Keyboard(undone).xcplaygroundpage/Contents.swift
@@ -31,9 +31,36 @@
 
 import Foundation
 
+/*
+ 
+ class Solution {
+     public int minSteps(int n) {
+         int ans = 0, d = 2;
+         while (n > 1) {
+             while (n % d == 0) {
+                 ans += d;
+                 n /= d;
+             }
+             d++;
+         }
+         return ans;
+     }
+ }
+
+ 
+ */
+
 class Solution {
     func minSteps(_ n: Int) -> Int {
-        return 0
+        var ans = 0, d = 2, n = n
+        while n > 1 {
+            while n % d == 0 {
+                ans += d
+                n /= d
+            }
+            d += 1
+        }
+        return ans
     }
 }
 
