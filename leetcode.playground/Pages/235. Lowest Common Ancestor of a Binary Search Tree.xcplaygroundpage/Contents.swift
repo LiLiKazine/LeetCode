1e7861c50b32316a97051a907df8772f5a6c29fd
yingkelei <yingkelei@pinduoduo.com>
2020-09-27 11:46:54 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ leetcode.playground/Pages/235. Lowest Common Ancestor of a Binary Search Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,98 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 235. Lowest Common Ancestor of a Binary Search Tree
+ 
+ Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ 
+ According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ 
+ Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ 
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ Output: 6
+ Explanation: The LCA of nodes 2 and 8 is 6.
+ Example 2:
+ 
+ Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ Output: 2
+ Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ 
+ 
+ Constraints:
+ 
+ All of the nodes' values will be unique.
+ p and q are different and both values will exist in the BST.
+ 
+ */
+
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+
+class Solution {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard var node = root, p != nil, q != nil else {
+            return nil
+        }
+        let vl = min(p!.val, q!.val), vr = max(q!.val, p!.val)
+        guard node.val != vl && node.val != vr else {
+            return node
+        }
+        while node.val < vl || node.val > vr {
+            if node.val < vl {
+                node = node.right!
+            } else if node.val > vr {
+                node = node.left!
+            } else {
+                break
+            }
+        }
+        
+        return node
+    }
+}
+
+//let solution = Solution()
+//let root = TreeNode(2)
+//root.left = TreeNode(1)
+//root.right = TreeNode(3)
+//let left = TreeNode(1)
+//let right = TreeNode(3)
+
+/*
+ 
+ [2,1,3]
+ 3
+ 1
+ 
+ [3,1,4,null,2]
+ 2
+ 4
+ 
+ [2,1]
+ 1
+ 2
+ 
+ [6,2,8,0,4,7,9,null,null,3,5]
+ 2
+ 4
+ */
+
+//: [Next](@next)
