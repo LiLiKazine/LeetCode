54e7a8e81217eb0128d612f900eb5b2f57ad3173
yingkelei <yingkelei@pinduoduo.com>
2020-12-11 17:46:50 +0800

    606. Construct String from Binary Tree

---
+++ leetcode.playground/Pages/606. Construct String from Binary Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,79 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 606. Construct String from Binary Tree
+ 
+ You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+
+ The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+
+ Example 1:
+ Input: Binary tree: [1,2,3,4]
+        1
+      /   \
+     2     3
+    /
+   4
+
+ Output: "1(2(4))(3)"
+
+ Explanation: Originallay it needs to be "1(2(4)())(3()())",
+ but you need to omit all the unnecessary empty parenthesis pairs.
+ And it will be "1(2(4))(3)".
+ Example 2:
+ Input: Binary tree: [1,2,3,null,4]
+        1
+      /   \
+     2     3
+      \
+       4
+
+ Output: "1(2()(4))(3)"
+
+ Explanation: Almost the same as the first example,
+ except we can't omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+ 
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+ 
+class Solution {
+    func tree2str(_ t: TreeNode?) -> String {
+        guard let node = t else {
+            return ""
+        }
+        let ans = recursive(node)
+        return ans
+    }
+    
+    func recursive(_ node: TreeNode) -> String {
+        var s = ""
+        s.append(String(node.val))
+        if node.left == nil && node.right == nil {
+            return s
+        }
+        s.append("(")
+        if let left = node.left {
+            s.append(recursive(left))
+        }
+        s.append(")")
+        if let right = node.right {
+            s.append("(")
+            s.append(recursive(right))
+            s.append(")")
+        }
+        return s
+    }
+}
+
+//: [Next](@next)
