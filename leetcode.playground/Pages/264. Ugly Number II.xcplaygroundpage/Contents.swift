6b40f56650e9fe49f4ea4034cb0d8480d009ae10
yingkelei <yingkelei@pinduoduo.com>
2021-04-11 11:31:14 +0800

    264. Ugly Number II

---
+++ leetcode.playground/Pages/264. Ugly Number II.xcplaygroundpage/Contents.swift
@@ -0,0 +1,88 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 264. Ugly Number II
+ 
+ Given an integer n, return the nth ugly number.
+
+ Ugly number is a positive number whose prime factors only include 2, 3, and/or 5.
+
+  
+
+ Example 1:
+
+ Input: n = 10
+ Output: 12
+ Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 ugly numbers.
+ Example 2:
+
+ Input: n = 1
+ Output: 1
+ Explanation: 1 is typically treated as an ugly number.
+  
+
+ Constraints:
+
+ 1 <= n <= 1690
+ 
+ */
+
+/*
+ 
+ class Solution {
+ public:
+     int nthUglyNumber(int n) {
+         vector<int> dp(n + 1);
+         dp[1] = 1;
+         int p2 = 1, p3 = 1, p5 = 1;
+         for (int i = 2; i <= n; i++) {
+             int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+             dp[i] = min(min(num2, num3), num5);
+             if (dp[i] == num2) {
+                 p2++;
+             }
+             if (dp[i] == num3) {
+                 p3++;
+             }
+             if (dp[i] == num5) {
+                 p5++;
+             }
+         }
+         return dp[n];
+     }
+ };
+ 
+ */
+
+class Solution {
+    func nthUglyNumber(_ n: Int) -> Int {
+        guard n > 1 else {
+            return 1
+        }
+        var dp = Array(repeating: 0, count: n + 1)
+        dp[1] = 1
+        var p2 = 1, p3 = 1, p5 = 1
+        for i in 2...n {
+            let num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5
+            dp[i] = min(min(num2, num3), num5)
+            if dp[i] == num2 {
+                p2 += 1
+            }
+            if dp[i] == num3 {
+                p3 += 1
+            }
+            if dp[i] == num5 {
+                p5 += 1
+            }
+        }
+        return dp[n]
+    }
+}
+
+let n = 10
+let solution = Solution()
+let ans = solution.nthUglyNumber(n)
+ans
+
+//: [Next](@next)
