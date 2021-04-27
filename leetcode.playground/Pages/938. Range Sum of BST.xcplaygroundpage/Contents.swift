cf6d31a33db93c0a1252d2812e6e5305e3147578
yingkelei <yingkelei@pinduoduo.com>
2021-04-27 11:24:26 +0800

    938. Range Sum of BST

---
+++ leetcode.playground/Pages/938. Range Sum of BST.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 938. Range Sum of BST
+ 
+ Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+
+  
+
+ Example 1:
+
+
+ Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+ Output: 32
+ Example 2:
+
+
+ Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+ Output: 23
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [1, 2 * 104].
+ 1 <= Node.val <= 105
+ 1 <= low <= high <= 105
+ All Node.val are unique.
+ 
+ */
+
+class Solution {
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        guard let node = root else {
+            return 0
+        }
+        if node.val < low {
+            return rangeSumBST(node.right, low, high)
+        } else  if node.val > high {
+            return rangeSumBST(node.left, low, high)
+        } else {
+            return node.val + rangeSumBST(node.left, low, high) + rangeSumBST(node.right, low, high)
+        }
+    }
+}
+
+//: [Next](@next)
