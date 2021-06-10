04df7bb8679f73d1f3b7336584916bf9313cfe74
yingkelei <yingkelei@pinduoduo.com>
2021-06-10 11:47:26 +0800

    518. Coin Change 2

---
+++ leetcode.playground/Pages/518. Coin Change 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 518. Coin Change 2
+ 
+ You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+ Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+ You may assume that you have an infinite number of each kind of coin.
+
+ The answer is guaranteed to fit into a signed 32-bit integer.
+
+  
+
+ Example 1:
+
+ Input: amount = 5, coins = [1,2,5]
+ Output: 4
+ Explanation: there are four ways to make up the amount:
+ 5=5
+ 5=2+2+1
+ 5=2+1+1+1
+ 5=1+1+1+1+1
+ Example 2:
+
+ Input: amount = 3, coins = [2]
+ Output: 0
+ Explanation: the amount of 3 cannot be made up just with coins of 2.
+ Example 3:
+
+ Input: amount = 10, coins = [10]
+ Output: 1
+  
+
+ Constraints:
+
+ 1 <= coins.length <= 300
+ 1 <= coins[i] <= 5000
+ All the values of coins are unique.
+ 0 <= amount <= 5000
+ 
+ */
+
+class Solution {
+    func change(_ amount: Int, _ coins: [Int]) -> Int {
+        var dp: [Int] = Array(repeating: 0, count: amount + 1)
+        dp[0] = 1
+        for coin in coins {
+            if coin > amount {
+                break
+            }
+            for i in coin...amount {
+                dp[i] += dp[i - coin]
+            }
+        }
+        return dp[amount]
+    }
+}
+
+//let amount = 10, coins = [10]
+let amount = 5, coins = [1,2,5]
+let solution = Solution()
+let ans = solution.change(amount, coins)
+ans
+
+
+//: [Next](@next)
