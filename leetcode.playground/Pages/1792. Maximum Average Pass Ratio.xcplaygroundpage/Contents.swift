4fef3eb9da6a4d0555762bda274dbe2107152ae1
lili <lilikazine@gmail.com>
2023-02-19 21:41:01 +0800

    1792. Maximum Average Pass Ratio

---
+++ leetcode.playground/Pages/1792. Maximum Average Pass Ratio.xcplaygroundpage/Contents.swift
@@ -0,0 +1,363 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 1792. Maximum Average Pass Ratio
+ There is a school that has classes of students and each class will be having a final exam. You are given a 2D integer array classes, where classes[i] = [passi, totali]. You know beforehand that in the ith class, there are totali total students, but only passi number of students will pass the exam.
+
+ You are also given an integer extraStudents. There are another extraStudents brilliant students that are guaranteed to pass the exam of any class they are assigned to. You want to assign each of the extraStudents students to a class in a way that maximizes the average pass ratio across all the classes.
+
+
+ Return the maximum possible average pass ratio after assigning the extraStudents students. Answers within 10-5 of the actual answer will be accepted.
+
+  
+
+ Example 1:
+
+ Input: classes = [[1,2],[3,5],[2,2]], extraStudents = 2
+ Output: 0.78333
+ Explanation: You can assign the two extra students to the first class. The average pass ratio will be equal to (3/4 + 3/5 + 2/2) / 3 = 0.78333.
+ Example 2:
+
+ Input: classes = [[2,4],[3,9],[4,5],[2,10]], extraStudents = 4
+ Output: 0.53485
+  
+
+ Constraints:
+
+ 1 <= classes.length <= 105
+ classes[i].length == 2
+ 1 <= passi <= totali <= 105
+ 1 <= extraStudents <= 105
+ 
+ */
+
+
+public struct Heap<T> {
+  
+  /** The array that stores the heap's nodes. */
+  var nodes = [T]()
+  
+  /**
+   * Determines how to compare two nodes in the heap.
+   * Use '>' for a max-heap or '<' for a min-heap,
+   * or provide a comparing method if the heap is made
+   * of custom elements, for example tuples.
+   */
+  private var orderCriteria: (T, T) -> Bool
+  
+  /**
+   * Creates an empty heap.
+   * The sort function determines whether this is a min-heap or max-heap.
+   * For comparable data types, > makes a max-heap, < makes a min-heap.
+   */
+  public init(sort: @escaping (T, T) -> Bool) {
+    self.orderCriteria = sort
+  }
+  
+  /**
+   * Creates a heap from an array. The order of the array does not matter;
+   * the elements are inserted into the heap in the order determined by the
+   * sort function. For comparable data types, '>' makes a max-heap,
+   * '<' makes a min-heap.
+   */
+  public init(array: [T], sort: @escaping (T, T) -> Bool) {
+    self.orderCriteria = sort
+    configureHeap(from: array)
+  }
+  
+  /**
+   * Configures the max-heap or min-heap from an array, in a bottom-up manner.
+   * Performance: This runs pretty much in O(n).
+   */
+  private mutating func configureHeap(from array: [T]) {
+    nodes = array
+    for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+      shiftDown(i)
+    }
+  }
+  
+  public var isEmpty: Bool {
+    return nodes.isEmpty
+  }
+  
+  public var count: Int {
+    return nodes.count
+  }
+  
+  /**
+   * Returns the index of the parent of the element at index i.
+   * The element at index 0 is the root of the tree and has no parent.
+   */
+  @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
+    return (i - 1) / 2
+  }
+  
+  /**
+   * Returns the index of the left child of the element at index i.
+   * Note that this index can be greater than the heap size, in which case
+   * there is no left child.
+   */
+  @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
+    return 2*i + 1
+  }
+  
+  /**
+   * Returns the index of the right child of the element at index i.
+   * Note that this index can be greater than the heap size, in which case
+   * there is no right child.
+   */
+  @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
+    return 2*i + 2
+  }
+  
+  /**
+   * Returns the maximum value in the heap (for a max-heap) or the minimum
+   * value (for a min-heap).
+   */
+  public func peek() -> T? {
+    return nodes.first
+  }
+  
+  /**
+   * Adds a new value to the heap. This reorders the heap so that the max-heap
+   * or min-heap property still holds. Performance: O(log n).
+   */
+  public mutating func insert(_ value: T) {
+    nodes.append(value)
+    shiftUp(nodes.count - 1)
+  }
+  
+  /**
+   * Adds a sequence of values to the heap. This reorders the heap so that
+   * the max-heap or min-heap property still holds. Performance: O(log n).
+   */
+  public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+    for value in sequence {
+      insert(value)
+    }
+  }
+  
+  /**
+   * Allows you to change an element. This reorders the heap so that
+   * the max-heap or min-heap property still holds.
+   */
+  public mutating func replace(index i: Int, value: T) {
+    guard i < nodes.count else { return }
+    
+    remove(at: i)
+    insert(value)
+  }
+  
+  /**
+   * Removes the root node from the heap. For a max-heap, this is the maximum
+   * value; for a min-heap it is the minimum value. Performance: O(log n).
+   */
+  @discardableResult public mutating func remove() -> T? {
+    guard !nodes.isEmpty else { return nil }
+    
+    if nodes.count == 1 {
+      return nodes.removeLast()
+    } else {
+      // Use the last node to replace the first one, then fix the heap by
+      // shifting this new first node into its proper position.
+      let value = nodes[0]
+      nodes[0] = nodes.removeLast()
+      shiftDown(0)
+      return value
+    }
+  }
+  
+  /**
+   * Removes an arbitrary node from the heap. Performance: O(log n).
+   * Note that you need to know the node's index.
+   */
+  @discardableResult public mutating func remove(at index: Int) -> T? {
+    guard index < nodes.count else { return nil }
+    
+    let size = nodes.count - 1
+    if index != size {
+      nodes.swapAt(index, size)
+      shiftDown(from: index, until: size)
+      shiftUp(index)
+    }
+    return nodes.removeLast()
+  }
+  
+  /**
+   * Takes a child node and looks at its parents; if a parent is not larger
+   * (max-heap) or not smaller (min-heap) than the child, we exchange them.
+   */
+  internal mutating func shiftUp(_ index: Int) {
+    var childIndex = index
+    let child = nodes[childIndex]
+    var parentIndex = self.parentIndex(ofIndex: childIndex)
+    
+    while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+      nodes[childIndex] = nodes[parentIndex]
+      childIndex = parentIndex
+      parentIndex = self.parentIndex(ofIndex: childIndex)
+    }
+    
+    nodes[childIndex] = child
+  }
+  
+  /**
+   * Looks at a parent node and makes sure it is still larger (max-heap) or
+   * smaller (min-heap) than its childeren.
+   */
+  internal mutating func shiftDown(from index: Int, until endIndex: Int) {
+    let leftChildIndex = self.leftChildIndex(ofIndex: index)
+    let rightChildIndex = leftChildIndex + 1
+    
+    // Figure out which comes first if we order them by the sort function:
+    // the parent, the left child, or the right child. If the parent comes
+    // first, we're done. If not, that element is out-of-place and we make
+    // it "float down" the tree until the heap property is restored.
+    var first = index
+    if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+      first = leftChildIndex
+    }
+    if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+      first = rightChildIndex
+    }
+    if first == index { return }
+    
+    nodes.swapAt(index, first)
+    shiftDown(from: first, until: endIndex)
+  }
+  
+  internal mutating func shiftDown(_ index: Int) {
+    shiftDown(from: index, until: nodes.count)
+  }
+  
+}
+
+
+
+// MARK: - Searching
+
+extension Heap where T: Equatable {
+  
+  /** Get the index of a node in the heap. Performance: O(n). */
+  public func index(of node: T) -> Int? {
+    return nodes.index(where: { $0 == node })
+  }
+  
+  /** Removes the first occurrence of a node from the heap. Performance: O(n). */
+  @discardableResult public mutating func remove(node: T) -> T? {
+    if let index = index(of: node) {
+      return remove(at: index)
+    }
+    return nil
+  }
+  
+}
+
+public struct PriorityQueue<T> {
+  fileprivate var heap: Heap<T>
+
+  /*
+    To create a max-priority queue, supply a > sort function. For a min-priority
+    queue, use <.
+  */
+  public init(sort: @escaping (T, T) -> Bool) {
+    heap = Heap(sort: sort)
+  }
+
+  public var isEmpty: Bool {
+    return heap.isEmpty
+  }
+
+  public var count: Int {
+    return heap.count
+  }
+
+  public func peek() -> T? {
+    return heap.peek()
+  }
+
+  public mutating func enqueue(_ element: T) {
+    heap.insert(element)
+  }
+
+  public mutating func dequeue() -> T? {
+    return heap.remove()
+  }
+
+  /*
+    Allows you to change the priority of an element. In a max-priority queue,
+    the new priority should be larger than the old one; in a min-priority queue
+    it should be smaller.
+  */
+  public mutating func changePriority(index i: Int, value: T) {
+    return heap.replace(index: i, value: value)
+  }
+}
+
+extension PriorityQueue where T: Equatable {
+  public func index(of element: T) -> Int? {
+    return heap.index(of: element)
+  }
+}
+class Solution {
+    func maxAverageRatio(_ classes: [[Int]], _ extraStudents: Int) -> Double {
+        var priorityQueue = PriorityQueue<[Int]> { c1, c2 in
+            let increase1 = Double(c1[0] + 1) / Double(c1[1] + 1) - Double(c1[0]) / Double(c1[1])
+            let increase2 = Double(c2[0] + 1) / Double(c2[1] + 1) - Double(c2[0]) / Double(c2[1])
+            return increase1 > increase2
+        }
+        for _class in classes {
+            priorityQueue.enqueue(_class)
+        }
+        var leftover = extraStudents
+        while leftover > 0 {
+            if var _class = priorityQueue.dequeue() {
+                 _class[0] += 1
+                _class[1] += 1
+                leftover -= 1
+                priorityQueue.enqueue(_class)
+            }
+        }
+        let classes = priorityQueue.heap.nodes
+        let totalRatio: Double = classes.reduce(0) { partialResult, _class in
+            return partialResult + Double(_class[0]) / Double(_class[1])
+        }
+        return totalRatio / Double(classes.count)
+    }
+}
+
+/*
+class Solution {
+    func maxAverageRatio(_ classes: [[Int]], _ extraStudents: Int) -> Double {
+        var classes = classes.sorted { c1, c2 in
+            let increase1 = Double(c1[0] + 1) / Double(c1[1] + 1) - Double(c1[0]) / Double(c1[1])
+            let increase2 = Double(c2[0] + 1) / Double(c2[1] + 1) - Double(c2[0]) / Double(c2[1])
+            return increase1 > increase2
+        }
+        var leftover = extraStudents
+        while leftover > 0 {
+            classes[0][0] += 1
+            classes[0][1] += 1
+            leftover -= 1
+            classes = classes.sorted { c1, c2 in
+                let increase1 = Double(c1[0] + 1) / Double(c1[1] + 1) - Double(c1[0]) / Double(c1[1])
+                let increase2 = Double(c2[0] + 1) / Double(c2[1] + 1) - Double(c2[0]) / Double(c2[1])
+                return increase1 > increase2
+            }
+        }
+        let totalRatio: Double = classes.reduce(0) { partialResult, _class in
+            return partialResult + Double(_class[0]) / Double(_class[1])
+        }
+        return totalRatio / Double(classes.count)
+    }
+}
+ */
+
+let classes = [[1,2],[3,5],[2,2]], extraStudents = 2
+let solution = Solution()
+let ans = solution.maxAverageRatio(classes, extraStudents)
+
+print(ans)
+
+//: [Next](@next)
