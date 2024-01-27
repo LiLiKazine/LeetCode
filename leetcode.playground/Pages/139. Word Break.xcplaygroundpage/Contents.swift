9fda2ae7964eac6cc38701dfcd7fa1d4c1d0d2ca
lili <lilikazine@gmail.com>
2024-01-27 15:09:49 +0800

    139. Word Break

---
+++ leetcode.playground/Pages/139. Word Break.xcplaygroundpage/Contents.swift
@@ -0,0 +1,62 @@
+//: [Previous](@previous)
+
+/*
+ 
+ 139. Word Break
+ 
+ Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+ Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+  
+
+ Example 1:
+
+ Input: s = "leetcode", wordDict = ["leet","code"]
+ Output: true
+ Explanation: Return true because "leetcode" can be segmented as "leet code".
+ Example 2:
+
+ Input: s = "applepenapple", wordDict = ["apple","pen"]
+ Output: true
+ Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ Note that you are allowed to reuse a dictionary word.
+ Example 3:
+
+ Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+ Output: false
+  
+
+ Constraints:
+
+ 1 <= s.length <= 300
+ 1 <= wordDict.length <= 1000
+ 1 <= wordDict[i].length <= 20
+ s and wordDict[i] consist of only lowercase English letters.
+ All the strings of wordDict are unique.
+ 
+ */
+
+import Foundation
+
+class Solution {
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        let wordDict = Set(wordDict)
+        
+        let s = s.map { String($0) }
+        var dp = Array(repeating: 0, count: s.count)
+        for i in 0..<s.count {
+            for j in 0...i {
+                let word = s[j...i].joined()
+                if wordDict.contains(word) {
+                    if j == 0 || dp[j-1] > 0 {
+                        dp[i] = 1
+                    }
+                }
+            }
+        }
+        return dp.last! > 0
+    }
+}
+
+//: [Next](@next)
