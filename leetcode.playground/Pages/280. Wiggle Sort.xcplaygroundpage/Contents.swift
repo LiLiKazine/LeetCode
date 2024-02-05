bee5eed4ab38d13b3abf8f8a73b1acde462afbb9
lili <lilikazine@gmail.com>
2024-02-05 11:30:22 +0800

    280. Wiggle Sort

---
+++ leetcode.playground/Pages/280. Wiggle Sort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: [Previous](@previous)
+
+/*
+ 280. Wiggle Sort
+ 
+ Given an integer array nums, reorder it such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+ You may assume the input array always has a valid answer.
+
+  
+
+ Example 1:
+
+ Input: nums = [3,5,2,1,6,4]
+ Output: [3,5,1,6,2,4]
+ Explanation: [1,6,2,5,3,4] is also accepted.
+ Example 2:
+
+ Input: nums = [6,6,5,6,3,8]
+ Output: [6,6,5,6,3,8]
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 5 * 104
+ 0 <= nums[i] <= 104
+ It is guaranteed that there will be an answer for the given input nums.
+  
+
+ Follow up: Could you solve the problem in O(n) time complexity?
+
+ */
+
+import Foundation
+
+class Solution {
+    func wiggleSort(_ nums: inout [Int]) {
+        for i in 1..<nums.count {
+            if i % 2 == 0 {
+                if nums[i] <= nums[i-1] {
+                    continue
+                }
+                
+            } else {
+                if nums[i] >= nums[i-1] {
+                    continue
+                }
+            }
+            let temp = nums[i]
+            nums[i] = nums[i-1]
+            nums[i-1] = temp
+        }
+    }
+}
+
+//: [Next](@next)
