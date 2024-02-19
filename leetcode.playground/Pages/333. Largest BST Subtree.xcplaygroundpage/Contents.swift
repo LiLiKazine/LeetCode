e4315176b72a46f0c3ba3eaf877c2efb842fb8be
lili <lilikazine@gmail.com>
2024-02-19 16:13:26 +0800

    333. Largest BST Subtree

---
+++ leetcode.playground/Pages/333. Largest BST Subtree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,76 @@
+//: [Previous](@previous)
+
+/*
+ 333. Largest BST Subtree
+ 中等
+ Given the root of a binary tree, find the largest
+ subtree
+ , which is also a Binary Search Tree (BST), where the largest means subtree has the largest number of nodes.
+
+ A Binary Search Tree (BST) is a tree in which all the nodes follow the below-mentioned properties:
+
+ The left subtree values are less than the value of their parent (root) node's value.
+ The right subtree values are greater than the value of their parent (root) node's value.
+ Note: A subtree must include all of its descendants.
+
+  
+
+ Example 1:
+
+
+
+ Input: root = [10,5,15,1,8,null,7]
+ Output: 3
+ Explanation: The Largest BST Subtree in this case is the highlighted one. The return value is the subtree's size, which is 3.
+ Example 2:
+
+ Input: root = [4,2,7,2,3,5,null,2,null,null,null,null,null,1]
+ Output: 2
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [0, 104].
+ -104 <= Node.val <= 104
+  
+
+ Follow up: Can you figure out ways to solve it with O(n) time complexity?
+
+ */
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    
+    func largestBSTSubtree(_ root: TreeNode?) -> Int {
+        let (ans, _, _, _) = find(root)
+        return ans
+    }
+    
+    func find(_ node: TreeNode?) -> (Int, Int, Int, Bool) {
+        guard let node else { return (0, Int.max, Int.min, true) }
+        
+        let (cntL, lowerL, upperL, isBstL) = find(node.left)
+        let (cntR, lowerR, upperR, isBstR) = find(node.right)
+        if node.val > upperL && isBstL && node.val < lowerR && isBstR {
+            return (cntL + cntR + 1, min(node.val, lowerL), max(node.val, upperR), true)
+        }
+        
+        return (max(cntL, cntR), 0, 0, false)
+    }
+}
+
+//: [Next](@next)
