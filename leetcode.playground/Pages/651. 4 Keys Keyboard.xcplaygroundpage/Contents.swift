3d987bbd7367be31ba3f04eeb377444efa517dd7
lili <lilikazine@gmail.com>
2024-03-02 11:41:37 +0800

    651. 4 Keys Keyboard

---
+++ leetcode.playground/Pages/651. 4 Keys Keyboard.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+//: [Previous](@previous)
+
+/*
+ 651. 4 Keys Keyboard
+ 中等
+ Imagine you have a special keyboard with the following keys:
+
+ A: Print one 'A' on the screen.
+ Ctrl-A: Select the whole screen.
+ Ctrl-C: Copy selection to buffer.
+ Ctrl-V: Print buffer on screen appending it after what has already been printed.
+ Given an integer n, return the maximum number of 'A' you can print on the screen with at most n presses on the keys.
+
+  
+
+ Example 1:
+
+ Input: n = 3
+ Output: 3
+ Explanation: We can at most get 3 A's on screen by pressing the following key sequence:
+ A, A, A
+ Example 2:
+
+ Input: n = 7
+ Output: 9
+ Explanation: We can at most get 9 A's on screen by pressing following key sequence:
+ A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
+  
+
+ Constraints:
+
+ 1 <= n <= 50
+ 
+ */
+
+import Foundation
+
+class Solution {
+    func maxA(_ n: Int) -> Int {
+        if n == 1 { return 1 }
+        var dp = Array(repeating: 0, count: n + 1)
+        dp[1] = 1
+        for i in 2...n {
+            dp[i] = dp[i-1] + 1
+            for j in 0...i-2 {
+                dp[i] = max(dp[i] + dp[j] * (i - 2 - j + 1))
+            }
+        }
+        return dp[n]
+    }
+}
+
+//: [Next](@next)
