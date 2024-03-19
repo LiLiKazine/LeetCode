a1646e3ab0af6b0d14043e3f26706ce18b0af362
lili <lilikazine@gmail.com>
2024-03-19 16:03:51 +0800

    210. Course Schedule II

--- leetcode.playground/Pages/207. Course Schedule.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/207. Course Schedule.xcplaygroundpage/Contents.swift
@@ -35,41 +35,6 @@
  
  */
 
-/*
- 
- class Solution {
-     public boolean canFinish(int numCourses, int[][] prerequisites) {
-         int[] indegrees = new int[numCourses];
-         List<List<Integer>> adjacency = new ArrayList<>();
-         Queue<Integer> queue = new LinkedList<>();
-         for(int i = 0; i < numCourses; i++)
-             adjacency.add(new ArrayList<>());
-         // Get the indegree and adjacency of every course.
-         for(int[] cp : prerequisites) {
-             indegrees[cp[0]]++;
-             adjacency.get(cp[1]).add(cp[0]);
-         }
-         // Get all the courses with the indegree of 0.
-         for(int i = 0; i < numCourses; i++)
-             if(indegrees[i] == 0) queue.add(i);
-         // BFS TopSort.
-         while(!queue.isEmpty()) {
-             int pre = queue.poll();
-             numCourses--;
-             for(int cur : adjacency.get(pre))
-                 if(--indegrees[cur] == 0) queue.add(cur);
-         }
-         return numCourses == 0;
-     }
- }
-
- 作者：jyd
- 链接：https://leetcode-cn.com/problems/course-schedule/solution/course-schedule-tuo-bu-pai-xu-bfsdfsliang-chong-fa/
- 来源：力扣（LeetCode）
- 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
- 
- */
-
 class Solution {
     
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
