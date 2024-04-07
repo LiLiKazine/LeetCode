de929a974a34961514b6ca5160cce18867dbd6bd
lili <lilikazine@gmail.com>
2024-04-07 16:17:32 +0800

    88. Merge Sorted Array

--- leetcode.playground/Pages/88. Merge Sorted Array.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/88. Merge Sorted Array.xcplaygroundpage/Contents.swift
@@ -32,21 +32,21 @@
 
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var i = m - 1, j = n - 1, k = m + n - 1
-        while i >= 0 && j >= 0 {
-            if nums1[i] > nums2[j] {
-                nums1[k] = nums1[i]
-                i -= 1
+        var m = m, n = n, i = m + n - 1
+        while m > 0 && n > 0 {
+            if nums1[m-1] > nums2[n-1] {
+                m -= 1
+                nums1[i] = nums1[m]
             } else {
-                nums1[k] = nums2[j]
-                j -= 1
+                n -= 1
+                nums1[i] = nums2[n]
             }
-            k -= 1
+            i-=1
         }
-        while j >= 0 {
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+        while n > 0 {
+            n -= 1
+            nums1[i] = nums2[n]
+            i -= 1
         }
     }
 }
