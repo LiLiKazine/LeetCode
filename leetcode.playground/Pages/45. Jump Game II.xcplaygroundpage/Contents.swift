f1e6be383f1298a5c9e437944fd8b27ceed1949d
lili <lilikazine@gmail.com>
2024-04-07 21:19:18 +0800

    45. Jump Game II

--- leetcode.playground/Pages/45. Jump Game II.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/45. Jump Game II.xcplaygroundpage/Contents.swift
@@ -25,49 +25,27 @@ import Foundation
 
 class Solution {
     func jump(_ nums: [Int]) -> Int {
-        let steps = greedy(nums)
+        let end = nums.count - 1
+        var cur = 0, steps = 0
+        var i = 0
+        while i < nums.count {
+            if cur >= end {
+                return steps
+            }
+            var next = cur
+            while i <= cur {
+                next = max(next, i + nums[i])
+                i += 1
+            }
+            if next > cur {
+                cur = next
+                steps += 1
+            }
+        }
         return steps
     }
-    
-    func greedy(_ nums: [Int]) -> Int {
-        var jumps = 0, curEnd = 0, farthest = 0
-        for i in 0..<nums.count-1 {
-            farthest = max(farthest, i + nums[i])
-            if i == curEnd {
-                jumps += 1
-                curEnd = farthest
-            }
-        }
-        return jumps
-    }
-    
-    func backTracing(_ heads: [Int], _ nums: [Int]) -> [Int] {
-        let target = nums.count - 1
-        if target == 0 {
-            return heads
-        }
-        let gap = target - nums[0]
-        if gap <= 0 {
-            return heads + [nums[0]]
-        }
-        if gap > 0 {
-            if nums[0] == 0 {
-                return []
-            }
-            var shortest: [Int] = []
-            for i in 1...nums[0] {
-                let tail = backTracing(heads + [nums[i]], Array(nums[i..<nums.count]))
-                if tail.count > 0 && (shortest.count == 0 || shortest.count > tail.count) {
-                    shortest = tail
-                }
-            }
-            if shortest.count > 0 {
-                return shortest
-            }
-        }
-        return []
-    }
 }
+
 let test = Solution()
 let result = test.jump([5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5])
 //: [Next](@next)
