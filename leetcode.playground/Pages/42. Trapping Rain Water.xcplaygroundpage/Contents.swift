2782b38cae00fea7e178ade650be4666d7ec7299
lili <lilikazine@gmail.com>
2024-04-08 11:51:46 +0800

    42. Trapping Rain Water

--- leetcode.playground/Pages/42. Trapping Rain Water.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/42. Trapping Rain Water.xcplaygroundpage/Contents.swift
@@ -19,54 +19,19 @@ import Foundation
 
 class Solution {
     func trap(_ height: [Int]) -> Int {
-        return dynamic(height)
-    }
-    
-    func brute(_ height: [Int]) -> Int {
-        var ans = 0
-        let size = height.count
-        if size == 0 {
-            return 0
+        guard height.count > 2 else { return 0 }
+        var left = Array(repeating: 0, count: height.count)
+        for i in 1..<height.count {
+            left[i] = max(height[i-1], left[i-1])
         }
-        for i in 0..<size-1 {
-            var maxLeft = 0, maxRight = 0, j = i, k = i
-            while j >= 0 {
-                maxLeft = max(maxLeft, height[j])
-                j-=1
-            }
-            while k < size {
-                maxRight = max(maxRight, height[k])
-                k+=1
-            }
-            ans += min(maxLeft, maxRight) - height[i]
-        }
-        return ans
-    }
-    
-    func dynamic(_ height: [Int]) -> Int {
-        let size = height.count
-        if size == 0 {
-            return 0
-        }
-        var ans = 0, maxLeft = Array(repeating: -1, count: size),
-        maxRight = Array(repeating: -1, count: size),
-        j = size-1
-        maxLeft[0] = height[0]
-        maxRight[size-1] = height[size-1]
-        for i in 1..<size {
-            maxLeft[i] = max(maxLeft[i-1], height[i])
-        }
-        
-        while j > 0 {
-            maxRight[j-1] = max(maxRight[j], height[j-1])
-            j-=1
-        }
-        for i in 0..<size {
-            ans += min(maxLeft[i], maxRight[i]) - height[i]
+        var right = height.last!, ans = 0
+        for i in stride(from: height.count - 2, through: 0, by: -1) {
+            let lev = height[i]
+            ans += max(0, min(right, left[i]) - lev)
+            right = max(right, height[i])
         }
         return ans
     }
 }
-let test = Solution()
-let result = test.dynamic([0,1,0,2,1,0,1,3,2,1,2,1])
+
 //: [Next](@next)
