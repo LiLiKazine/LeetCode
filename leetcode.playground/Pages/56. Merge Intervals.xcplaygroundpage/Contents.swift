143fc54ed408e5fd5322dd34c4cf55d9403e2172
lili <lilikazine@gmail.com>
2024-05-09 17:12:38 +0800

    56. Merge Intervals

---
+++ leetcode.playground/Pages/56. Merge Intervals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+//: [Previous](@previous)
+
+/*
+ 56. Merge Intervals
+ 中等
+ Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+  
+
+ Example 1:
+
+ Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+ Output: [[1,6],[8,10],[15,18]]
+ Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ Example 2:
+
+ Input: intervals = [[1,4],[4,5]]
+ Output: [[1,5]]
+ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+  
+
+ Constraints:
+
+ 1 <= intervals.length <= 104
+ intervals[i].length == 2
+ 0 <= starti <= endi <= 104
+ 
+ */
+
+import Foundation
+
+class Solution {
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        var intervals = intervals.sorted { a, b in
+            return a[0] < b[0]
+        }
+        var i = 1
+        while i < intervals.count {
+            var prev = intervals[i-1]
+            let cur = intervals[i]
+            if cur[0] > prev[1] {
+                i += 1
+                continue
+            }
+            prev[1] = max(prev[1], cur[1])
+            intervals[i-1] = prev
+            intervals.remove(at: i)
+        }
+        return intervals
+    }
+}
+
+//: [Next](@next)
