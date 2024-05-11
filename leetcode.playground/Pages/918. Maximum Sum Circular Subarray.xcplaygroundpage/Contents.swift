f9b9235d863ff2f47a7160324376309cd03e3436
lili <lilikazine@gmail.com>
2024-05-11 11:35:55 +0800

    918. Maximum Sum Circular Subarray

---
+++ leetcode.playground/Pages/918. Maximum Sum Circular Subarray.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+//: [Previous](@previous)
+
+/*
+ 918. Maximum Sum Circular Subarray
+ 中等
+ Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+ A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+ A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,-2,3,-2]
+ Output: 3
+ Explanation: Subarray [3] has maximum sum 3.
+ Example 2:
+
+ Input: nums = [5,-3,5]
+ Output: 10
+ Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+ Example 3:
+
+ Input: nums = [-3,-2,-3]
+ Output: -2
+ Explanation: Subarray [-2] has maximum sum -2.
+  
+
+ Constraints:
+
+ n == nums.length
+ 1 <= n <= 3 * 104
+ -3 * 104 <= nums[i] <= 3 * 104
+ 
+ */
+
+import Foundation
+
+class Solution {
+    func maxSubarraySumCircular(_ nums: [Int]) -> Int {
+        
+        if nums.isEmpty { return 0 }
+        
+        let n = nums.count
+        
+        var pre = nums[0], ans = nums[0]
+        var queue = [[0, pre]]
+        
+        for i in 1..<n*2 {
+            let num = nums[i % n]
+            pre += num
+            
+            while let top = queue.first, top[0] + n < i {
+                queue.removeFirst()
+            }
+            if let top = queue.first {
+                ans = max(pre - top[1], ans)
+            }
+            while let last = queue.last, last[1] > pre {
+                queue.removeLast()
+            }
+            queue.append([i, pre])
+        }
+        return ans
+    }
+}
+
+//: [Next](@next)
