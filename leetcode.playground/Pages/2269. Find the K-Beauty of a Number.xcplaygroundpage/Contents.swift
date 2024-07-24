d0cb71fdae604b68cb1776aca9d2d46a071938d5
lili <lilikazine@gmail.com>
2024-07-24 21:42:28 +0800

    2269. Find the K-Beauty of a Number

---
+++ leetcode.playground/Pages/2269. Find the K-Beauty of a Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+//: [Previous](@previous)
+
+/*
+ 2269. Find the K-Beauty of a Number
+ 简单
+ The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
+
+ It has a length of k.
+ It is a divisor of num.
+ Given integers num and k, return the k-beauty of num.
+
+ Note:
+
+ Leading zeros are allowed.
+ 0 is not a divisor of any value.
+ A substring is a contiguous sequence of characters in a string.
+
+  
+
+ Example 1:
+
+ Input: num = 240, k = 2
+ Output: 2
+ Explanation: The following are the substrings of num of length k:
+ - "24" from "240": 24 is a divisor of 240.
+ - "40" from "240": 40 is a divisor of 240.
+ Therefore, the k-beauty is 2.
+ Example 2:
+
+ Input: num = 430043, k = 2
+ Output: 2
+ Explanation: The following are the substrings of num of length k:
+ - "43" from "430043": 43 is a divisor of 430043.
+ - "30" from "430043": 30 is not a divisor of 430043.
+ - "00" from "430043": 0 is not a divisor of 430043.
+ - "04" from "430043": 4 is not a divisor of 430043.
+ - "43" from "430043": 43 is a divisor of 430043.
+ Therefore, the k-beauty is 2.
+  
+
+ Constraints:
+
+ 1 <= num <= 109
+ 1 <= k <= num.length (taking num as a string)
+ 
+ */
+
+import Foundation
+
+class Solution {
+    func divisorSubstrings(_ num: Int, _ k: Int) -> Int {
+        var arr = Array(String(num)), ans: Int = 0
+        
+        for i in k-1..<arr.count {
+            
+            let j = i - k + 1
+            let str = String(arr[j...i])
+            
+            guard let val = Int(str), val != 0 else {
+                continue
+            }
+            if num % val == 0 {
+                ans += 1
+            }
+        }
+        return ans
+    }
+}
+
+//: [Next](@next)
