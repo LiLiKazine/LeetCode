280cd63d8a0f361511dbb5b63c6f2ef6385a05e2
lili <lilikazine@gmail.com>
2024-07-28 21:16:35 +0800

    904. Fruit Into Baskets

--- leetcode.playground/Pages/904. Fruit Into Baskets.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/904. Fruit Into Baskets.xcplaygroundpage/Contents.swift
@@ -50,83 +50,23 @@
  
  */
 
-/*
- class Solution {
-     public int totalFruit(int[] tree) {
-         int ans = 0, i = 0;
-         Counter count = new Counter();
-         for (int j = 0; j < tree.length; ++j) {
-             count.add(tree[j], 1);
-             while (count.size() >= 3) {
-                 count.add(tree[i], -1);
-                 if (count.get(tree[i]) == 0)
-                     count.remove(tree[i]);
-                 i++;
-             }
-
-             ans = Math.max(ans, j - i + 1);
-         }
-
-         return ans;
-     }
- }
-
- class Counter extends HashMap<Integer, Integer> {
-     public int get(int k) {
-         return containsKey(k) ? super.get(k) : 0;
-     }
-
-     public void add(int k, int v) {
-         put(k, get(k) + v);
-     }
- }
-
- 作者：LeetCode
- 链接：https://leetcode-cn.com/problems/fruit-into-baskets/solution/shui-guo-cheng-lan-by-leetcode/
- 来源：力扣（LeetCode）
- 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
- 
- */
-
-/*
- 
- class Solution {
- public:
-     int totalFruit(vector<int>& tree) {
-         int K = 2;
-         int i = 0, j = 0, max_res = 0;
-         unordered_map<int, int> map;
-         for (i = 0; i < tree.size(); i++) {
-              map[tree[i]]++;
-              while(map.size() > K) {
-                  max_res = max(max_res,  i - j);
-                  map[tree[j]]--;
-                  if(map[tree[j]] == 0) map.erase(tree[j]);
-                  j++;
-              }
-         }
-         max_res = max(max_res,  i - j);
-         return max_res;
-     }
- };
- 
- */
-
 class Solution {
     func totalFruit(_ tree: [Int]) -> Int {
-        var j = 0, ans = 0, cache: [Int: Int] = [:]
-        for i in 0..<tree.count {
-            cache[tree[i]] = (cache[tree[i]] ?? 0) + 1
-            while cache.keys.count > 2 {
-                ans = max(ans, i - j)
-                cache[tree[j]]! -= 1;
-                if cache[tree[j]]! == 0 {
-                    cache.removeValue(forKey: tree[j])
-                }
-                j += 1
+        var ans = 1, start = 0, firstEnd = 0, secondEnd = -1
+        for i in 1..<tree.count {
+            let fruit = tree[i]
+            if tree[start] == fruit {
+                firstEnd = i
+            } else if secondEnd < 0 || tree[secondEnd] == fruit {
+                secondEnd = i
+            } else {
+                start = min(firstEnd, secondEnd) + 1
+                firstEnd = i - 1
+                secondEnd = i
             }
+            ans = max(ans, i - start + 1)
         }
-        ans = max(ans, tree.count - j)
+        
         return ans
     }
 }
