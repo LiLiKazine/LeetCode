fef1d458fca94c53b5487e98082f54751369a60d
lili <lilikazine@gmail.com>
2024-09-05 10:42:40 +0800

    3174. Clear Digits

---
+++ leetcode.playground/Pages/3174. Clear Digits.xcplaygroundpage/Contents.swift
@@ -0,0 +1,62 @@
+//: [Previous](@previous)
+
+/*
+ 3174. Clear Digits
+ 简单
+ You are given a string s.
+
+ Your task is to remove all digits by doing this operation repeatedly:
+
+ Delete the first digit and the closest non-digit character to its left.
+ Return the resulting string after removing all digits.
+
+  
+
+ Example 1:
+
+ Input: s = "abc"
+
+ Output: "abc"
+
+ Explanation:
+
+ There is no digit in the string.
+
+ Example 2:
+
+ Input: s = "cb34"
+
+ Output: ""
+
+ Explanation:
+
+ First, we apply the operation on s[2], and s becomes "c4".
+
+ Then we apply the operation on s[1], and s becomes "".
+
+  
+
+ Constraints:
+
+ 1 <= s.length <= 100
+ s consists only of lowercase English letters and digits.
+ The input is generated such that it is possible to delete all digits.
+ */
+
+import Foundation
+
+class Solution {
+    func clearDigits(_ s: String) -> String {
+        var chars = [Character]()
+        for c in s {
+            if let _ = Int(String(c)) {
+                chars.removeLast()
+            } else {
+                chars.append(c)
+            }
+        }
+        return String(chars)
+    }
+}
+
+//: [Next](@next)
