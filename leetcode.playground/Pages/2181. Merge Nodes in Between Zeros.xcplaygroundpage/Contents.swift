3b9a6e4296a4d3dc94d8941c7857d125f1fd675f
lili <lilikazine@gmail.com>
2024-09-09 10:56:28 +0800

    2181. Merge Nodes in Between Zeros

---
+++ leetcode.playground/Pages/2181. Merge Nodes in Between Zeros.xcplaygroundpage/Contents.swift
@@ -0,0 +1,76 @@
+//: [Previous](@previous)
+
+/*
+ 2181. Merge Nodes in Between Zeros
+ 中等
+ You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+ For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+ Return the head of the modified linked list.
+
+  
+
+ Example 1:
+
+
+ Input: head = [0,3,1,0,4,5,2,0]
+ Output: [4,11]
+ Explanation:
+ The above figure represents the given linked list. The modified list contains
+ - The sum of the nodes marked in green: 3 + 1 = 4.
+ - The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+ Example 2:
+
+
+ Input: head = [0,1,0,3,0,2,2,0]
+ Output: [1,3,4]
+ Explanation:
+ The above figure represents the given linked list. The modified list contains
+ - The sum of the nodes marked in green: 1 = 1.
+ - The sum of the nodes marked in red: 3 = 3.
+ - The sum of the nodes marked in yellow: 2 + 2 = 4.
+  
+
+ Constraints:
+
+ The number of nodes in the list is in the range [3, 2 * 105].
+ 0 <= Node.val <= 1000
+ There are no two consecutive nodes with Node.val == 0.
+ The beginning and end of the linked list have Node.val == 0.
+ 
+ */
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func mergeNodes(_ head: ListNode?) -> ListNode? {
+        var sentry = ListNode(0)
+        sentry.next = head
+        var cur: ListNode? = sentry
+        while let next = cur?.next {
+            var tail = next.next
+            while let node = tail, node.val != 0 {
+                next.val += node.val
+                tail = tail?.next
+            }
+            if next.val != 0 {
+                next.next = tail
+                cur = next
+            } else {
+                cur?.next = tail
+            }
+        }
+        return sentry.next
+    }
+}
+
+//: [Next](@next)
