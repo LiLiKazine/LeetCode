d4ec908b92843ded5dc2f81a3af2108a1c0466a7
lili <lilikazine@gmail.com>
2024-10-27 11:23:47 +0800

    1482. Minimum Number of Days to Make m Bouquets

--- leetcode.playground/Pages/1482. Minimum Number of Days to Make m Bouquets(undone).xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/1482. Minimum Number of Days to Make m Bouquets(undone).xcplaygroundpage/Contents.swift
@@ -59,55 +59,33 @@
  
  */
 
-/*
- 
- class Solution {
- public:
-     int minDays(vector<int>& bloomDay, int m, int k) {
-         if (m > bloomDay.size() / k) {
-             return -1;
-         }
-         int low = INT_MAX, high = 0;
-         int length = bloomDay.size();
-         for (int i = 0; i < length; i++) {
-             low = min(low, bloomDay[i]);
-             high = max(high, bloomDay[i]);
-         }
-         while (low < high) {
-             int days = (high - low) / 2 + low;
-             if (canMake(bloomDay, days, m, k)) {
-                 high = days;
-             } else {
-                 low = days + 1;
-             }
-         }
-         return low;
-     }
-
-     bool canMake(vector<int>& bloomDay, int days, int m, int k) {
-         int bouquets = 0;
-         int flowers = 0;
-         int length = bloomDay.size();
-         for (int i = 0; i < length && bouquets < m; i++) {
-             if (bloomDay[i] <= days) {
-                 flowers++;
-                 if (flowers == k) {
-                     bouquets++;
-                     flowers = 0;
-                 }
-             } else {
-                 flowers = 0;
-             }
-         }
-         return bouquets >= m;
-     }
- };
- 
- */
-
 class Solution {
     func minDays(_ bloomDay: [Int], _ m: Int, _ k: Int) -> Int {
-        return 0
+        guard m * k <= bloomDay.count else { return -1 }
+        var l = 1, r = bloomDay.max()!
+        var ans = r
+        while l <= r {
+            let mid = l + (r - l) / 2
+            var bouquets = 0, flowers = 0
+            for b in bloomDay {
+                if b <= mid {
+                    flowers += 1
+                } else {
+                    flowers = 0
+                }
+                if flowers == k {
+                    bouquets += 1
+                    flowers = 0
+                }
+            }
+            if bouquets >=  m {
+                ans = mid
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+        return ans
     }
 }
 
