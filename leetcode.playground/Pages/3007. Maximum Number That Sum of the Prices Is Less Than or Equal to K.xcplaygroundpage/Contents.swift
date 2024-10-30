ca17a37c6edc6e91836005f19818a144b64a4b27
lili <lilikazine@gmail.com>
2024-10-30 20:51:29 +0800

    3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K

--- leetcode.playground/Pages/3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K.xcplaygroundpage/Contents.swift
@@ -69,103 +69,51 @@
 
 import Foundation
 
-/*
- class Solution {
-     public long findMaximumNumber(long k, int x) {
-         this.x = x;
-         // 开区间二分，原理见 https://www.bilibili.com/video/BV1AP41137w7/
-         long left = 0;
-         long right = (k + 1) << x;
-         while (left + 1 < right) {
-             long mid = (left + right) >>> 1;
-             if (countDigitOne(mid) <= k) {
-                 left = mid;
-             } else {
-                 right = mid;
-             }
-         }
-         return left;
-     }
-
-     private int x;
-     private long num;
-     private long memo[][];
-
-     private long countDigitOne(long num) {
-         this.num = num;
-         int m = 64 - Long.numberOfLeadingZeros(num);
-         memo = new long[m][m + 1];
-         for (long[] row : memo) {
-             Arrays.fill(row, -1);
-         }
-         return dfs(m - 1, 0, true);
-     }
-
-     private long dfs(int i, int cnt1, boolean isLimit) {
-         if (i < 0) {
-             return cnt1;
-         }
-         if (!isLimit && memo[i][cnt1] != -1) {
-             return memo[i][cnt1];
-         }
-         int up = isLimit ? (int) (num >> i & 1) : 1;
-         long res = 0;
-         for (int d = 0; d <= up; d++) { // 枚举要填入的数字 d
-             res += dfs(i - 1, cnt1 + (d == 1 && (i + 1) % x == 0 ? 1 : 0), isLimit && d == up);
-         }
-         if (!isLimit) {
-             memo[i][cnt1] = res;
-         }
-         return res;
-     }
- }
-
-*/
-
-
 class Solution {
     
-    var x: Int = 0
-    var memo = [[Int]]()
-    var num: Int = 0
-    
+    struct Key : Hashable {
+        let i: Int
+        let isLimited: Bool
+        let cnt: Int
+    }
+
     func findMaximumNumber(_ k: Int, _ x: Int) -> Int {
-        self.x = x
+        
+        func countDigitOne(_ n: Int) -> Int {
+            var cache = [Key : Int]()
+            let len = Int.bitWidth - n.leadingZeroBitCount
+            func dfs(_ i: Int, _ isLimited: Bool, _ cnt: Int) -> Int {
+                if i == 0 {
+                    return cnt
+                }
+                let key = Key(i: i, isLimited: isLimited, cnt: cnt)
+                if let value = cache[key] {
+                    return value
+                }
+                let up = isLimited ? n >> (i - 1) & 1 : 1
+                var res = 0
+                for j in 0...up {
+                    let valid = (i % x == 0) && j == 1
+                    res += dfs(i - 1, isLimited && j == up, cnt + (valid ? 1 : 0))
+                }
+                cache[key] = res
+                return res
+            }
+
+            let res = dfs(len, true, 0)
+            return res
+        }
+        
         var l = 0, r = (k + 1) << x
         while l + 1 < r {
-            var mid = l + (r - l) / 2
+            let mid = l + (r - l) / 2
             if countDigitOne(mid) <= k {
-                l = mid;
+                l = mid
             } else {
-                r = mid;
+                r = mid
             }
         }
         return l
-    }
-    
-    func countDigitOne(_ num: Int) -> Int {
-        self.num = num
-        let m = 64 - num.leadingZeroBitCount
-        memo = Array(repeating: Array(repeating: -1, count: m + 1), count: m)
-        return dfs(m - 1, 0, true)
-    }
-
-    func dfs(_ i : Int, _ cnt1: Int, _ isLimit: Bool) -> Int {
-        if i < 0 {
-            return cnt1
-        }
-        if (!isLimit && memo[i][cnt1] != -1) {
-            return memo[i][cnt1]
-        }
-        let up = isLimit ? (num >> i & i) : 1
-        var res = 0
-        for d in 0...up {
-            res += dfs(i - 1, cnt1 + (d == 1 && (i + 1) % x == 0 ? 1 : 0), isLimit && d == up)
-        }
-        if !isLimit {
-            memo[i][cnt1] = res
-        }
-        return cnt1
     }
 }
 
