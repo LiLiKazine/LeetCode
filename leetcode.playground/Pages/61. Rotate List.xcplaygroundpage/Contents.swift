baa3c6fb5c6905f345de5be316c6c0fedaa06a58
lili <lilikazine@gmail.com>
2024-11-26 20:42:24 +0800

    61. Rotate List

--- leetcode.playground/Pages/61. Rotate List.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/61. Rotate List.xcplaygroundpage/Contents.swift
@@ -30,22 +30,32 @@
 
 class Solution {
     func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
-        var nodes: [ListNode] = [], node = head
-        while node != nil {
-            nodes.append(node!)
-            node = node?.next
+        var n = 0, cur = head
+        while cur != nil {
+            n += 1
+            cur = cur?.next
         }
-        if nodes.count <= 1 {
+        if n == 0 {
+            return nil
+        }
+        let k = k % n
+        if k == 0 {
             return head
         }
-        let count = nodes.count, offset = k % count
-        if offset == 0 {
-            return head
+        var tail = head
+        for _ in 0..<k {
+            tail = tail?.next
         }
-        let newHead = nodes[count - offset]
-        nodes.last?.next = head
-        nodes[count - offset - 1].next = nil
-        return newHead
+        let sentry = ListNode(0, head)
+        var p = head
+        while tail?.next != nil {
+            tail = tail?.next
+            p = p?.next
+        }
+        tail?.next = head
+        sentry.next = p?.next
+        p?.next = nil
+        return sentry.next
     }
 }
 
