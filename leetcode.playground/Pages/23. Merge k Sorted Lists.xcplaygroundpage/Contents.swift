40ca03e510ed243b444084f0083e68a7c45a0456
lili <lilikazine@gmail.com>
2024-11-28 21:11:10 +0800

    23. Merge k Sorted Lists

--- leetcode.playground/Pages/23. Merge k Sorted Lists.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/23. Merge k Sorted Lists.xcplaygroundpage/Contents.swift
@@ -31,86 +31,44 @@ public class ListNode {
 
 class Solution {
     func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
-//        var filtered = lists.compactMap{ $0 }
-//        return stupid(&filtered)
-//        var ans: ListNode?
-//        for i in 0..<lists.count {
-//            var node = lists[i]
-//            ans = merge(&ans, &node)
-//        }
-//        return ans
-        var variable = lists
-        return divide(&variable)
-    }
-    
-    func divide( _ lists: inout [ListNode?]) -> ListNode? {
-        if lists.count < 2 {
-            return lists.first ?? nil
-        }
-        if lists.count == 2 {
-            var node1 = lists[0], node2 = lists[1]
-            return merge(&node1, &node2)
-        }
-        var nextLists: [ListNode?] = []
-        while !lists.isEmpty {
-            var n1 = lists.popLast() ?? nil
-            var n2 = lists.popLast() ?? nil
-            nextLists.append(merge(&n1, &n2))
-        }
-        return divide(&nextLists)
-    }
-    
-    func merge(_ node1: inout ListNode?, _ node2: inout ListNode?) -> ListNode? {
-        if node1 == nil || node2 == nil {
-            return node1 == nil ? node2 : node1
-        }
-        var head = ListNode(0), tail = head
-        while node1 != nil || node2 != nil {
-            if let v1 = node1?.val, let v2 = node2?.val {
-                if v1 > v2 {
-                    tail.next = ListNode(v2)
-                    node2 = node2?.next
-                } else {
-                    tail.next = ListNode(v1)
-                    node1 = node1?.next
-                }
-                tail = tail.next!
-            } else {
-                if node1 == nil {
-                    tail.next = node2
-                } else {
-                    tail.next = node1
-                }
+
+        func merge(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let sentry = ListNode(0)
+        var cur: ListNode? = sentry, l1 = l1, l2 = l2
+        while l1 != nil || l2 != nil {
+            if l1 == nil {
+                cur?.next = l2
                 break
+            } else if l2 == nil {
+                cur?.next = l1
+                break
+            } else if l1!.val < l2!.val {
+                cur?.next = l1
+                l1 = l1?.next
+                cur = cur?.next
+            } else {
+                cur?.next = l2
+                l2 = l2?.next
+                cur = cur?.next
             }
         }
-        return head.next
+        return sentry.next
     }
-    
-    func stupid(_ lists: inout [ListNode]) -> ListNode? {
-        var head: ListNode?, tail: ListNode?
-        while !lists.isEmpty {
-            var idx = 0
-            for i in 0..<lists.count {
-                let val = lists[i].val
-                if val < lists[idx].val {
-                    idx = i
-                }
+        func merge(_ range: Range<Int>) -> ListNode? {
+            let i = range.lowerBound, j = range.upperBound
+            if i == j {
+                return nil
             }
-            if head == nil {
-                head = ListNode(lists[idx].val)
-                tail = head
-            } else {
-                tail?.next = ListNode(lists[idx].val)
-                tail = tail?.next
+            if j - i == 1 {
+                return lists[i]
             }
-            if let next = lists[idx].next {
-                lists[idx] = next
-            } else {
-                lists.remove(at: idx)
+            if j - i == 2 {
+                return merge(lists[i], lists[i+1])
             }
+            let mid = i + (j - i) / 2
+            return merge(merge(i..<mid), merge(mid..<j))
         }
-        return head
+        return merge(0..<lists.count)
     }
 }
 
