010bb84deea60d26929d43fc2b71af54fc13a0b7
lili <lilikazine@gmail.com>
2024-12-05 11:39:36 +0800

    110. Balanced Binary Tree

---
+++ leetcode.playground/Pages/110. Balanced Binary Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: [Previous](@previous)
+
+/*
+ 110. Balanced Binary Tree
+ 简单
+ Given a binary tree, determine if it is
+ height-balanced
+ .
+
+  
+
+ Example 1:
+
+
+ Input: root = [3,9,20,null,null,15,7]
+ Output: true
+ Example 2:
+
+
+ Input: root = [1,2,2,3,3,null,null,4,4]
+ Output: false
+ Example 3:
+
+ Input: root = []
+ Output: true
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [0, 5000].
+ -104 <= Node.val <= 104
+ */
+
+import Foundation
+
+class Solution {
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        func dfs(_ p: TreeNode?) -> Int {
+            guard let p else {
+                return 0
+            }
+            let l = dfs(p.left)
+            if l == -1 {
+                return -1
+            }
+            let r = dfs(p.right)
+            if r == -1 {
+                return -1
+            }
+            return abs(r - l) > 1 ? -1 : max(r, l) + 1
+        }
+        return dfs(root) != -1
+    }
+}
+
+//: [Next](@next)
