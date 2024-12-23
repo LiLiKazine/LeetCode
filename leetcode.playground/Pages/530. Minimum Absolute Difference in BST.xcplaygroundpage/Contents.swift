4ff9598368c2bc8e62661cbeeeeb3caafeeb0434
lili <lilikazine@gmail.com>
2024-12-23 16:39:14 +0800

    530. Minimum Absolute Difference in BST

--- leetcode.playground/Pages/530. Minimum Absolute Difference in BST.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/530. Minimum Absolute Difference in BST.xcplaygroundpage/Contents.swift
@@ -30,53 +30,23 @@
  
  */
 
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
-    }
-}
-
 class Solution {
     func getMinimumDifference(_ root: TreeNode?) -> Int {
-        var node = root, stack = [TreeNode?](), list = [Int](), ans = Int.max
-        enstack(root, &stack)
-        while !stack.isEmpty {
-            node = stack.removeLast()
-            list.append(node!.val)
-            if let right = node?.right {
-                enstack(right, &stack)
+        var ans = Int.max, prev: Int? = nil
+        
+        func dfs(_ p: TreeNode?)  {
+            guard let p else { return }
+            dfs(p.left)
+            if let prev {
+                ans = min(abs(prev - p.val), ans)
             }
-        }
-        print(list)
-        for i in 1..<list.count {
-            ans = min(ans, list[i] - list[i-1])
-        }
+            prev = p.val
+            dfs(p.right)
+        }        
+        dfs(root)
         return ans
-    }
-    
-    func enstack(_ node: TreeNode?, _ stack: inout [TreeNode?]) {
-        guard var node = node else {
-            return
-        }
-        stack.append(node)
-        while node.left != nil {
-            node = node.left!
-            stack.append(node)
-        }
     }
 }
 
-let root = TreeNode(1)
-root.right = TreeNode(3)
-root.right?.left = TreeNode(2)
-
-let solution = Solution()
-let ans = solution.getMinimumDifference(root)
-ans
 
 //: [Next](@next)
