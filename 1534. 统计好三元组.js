6c9ead345f80e14844ea50a70f4a5d0fadcf4e81
li.sheng <li.sheng@notanemail.com>
2025-06-04 15:47:08 +0800

    basics

---
+++ 1534. 统计好三元组.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function(arr, a, b, c) {
+    var ans = 0;
+    for (let i = 0, len = arr.length; i < len - 2; i++) {
+        for (let j = i + 1; j < len - 1; j++) {
+            for (let k = j + 1; k < len; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a &&
+                    Math.abs(arr[j] - arr[k]) <= b &&
+                    Math.abs(arr[i] - arr[k]) <= c) {
+                    ans++;
+                }
+            }
+        }
+    }
+
+    return ans;
+};
